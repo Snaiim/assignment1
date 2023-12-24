b0b40731081a01bc2ee775ba893a2d9ae717be88
Admin <satanovaa06@gmail.com>
2023-12-24 23:02:48 +0600

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,36 @@
+import model.*;
+import model.Point;
+
+import java.awt.*;
+import java.awt.geom.AffineTransform;
+import java.awt.geom.PathIterator;
+import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Main {
+    private static Scanner sc;
+
+    static {
+        try {
+            sc = new Scanner(new File("C:\\Users\\PC\\IdeaProjects\\Assignment1\\src\\source"));
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Shape shape = new Shape();
+        while (sc.hasNextLine()) {
+            double x = sc.nextDouble();
+            double y = sc.nextDouble();
+            shape.addPoint(new Point(x, y));
+        }
+        System.out.println(shape.calculatePerimeter());
+        System.out.println(shape.getLongest());
+        System.out.println(shape.getAverageSide());
     }
 }
+
